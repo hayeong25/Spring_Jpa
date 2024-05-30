31a2eaae0cdc462ddbc3d92d7884c8b1bd68cffc
hayeong25 <hayeong0624@gmail.com>
2024-05-30 10:13:29 +0900

    style: 오타 수정

--- src/main/java/com/test/jpa/repository/MemoRepository.java
+++ src/main/java/com/test/jpa/repository/MemoRepository.java
@@ -14,20 +14,20 @@ import java.util.List;
 // @Query : JPA가 제공하는 쿼리 메소드 이외의 처리 원할 시 사용
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
-    @Query("SELECT m FROM MEMO m ORDER BY m.mno DESC")
+    @Query("SELECT m FROM Memo m ORDER BY m.mno DESC")
     List<Memo> getListDesc();
 
     // @Query 파라미터 바인딩
     @Transactional
     @Modifying
-    @Query("UPDATE MEMO m SET m.memo =: memo WHERE m.mno = :mno")
+    @Query("UPDATE Memo m SET m.memo = :memo WHERE m.mno = :mno")
     int updateMemo(@Param("mno") Long mno, @Param("memo") String memo);
 
     // @Query 페이징 처리
-    @Query(value = "SELECT m FROM MEMO m WHERE m.mno > :mno", countQuery = "SELECT COUNT(m) FROM MEMO m WHERE m.mno > :mno")
+    @Query(value = "SELECT m FROM Memo m WHERE m.mno > :mno", countQuery = "SELECT COUNT(m) FROM Memo m WHERE m.mno > :mno")
     Page<Memo> getListWithQuery(Long mno, Pageable pageable);
 
     // Native SQL
-    @Query(value = "SELECT * FROM MEMO WHERE mno > 0", nativeQuery = true)
+    @Query(value = "SELECT * FROM Memo WHERE mno > 0", nativeQuery = true)
     List<Object[]> getNativeResult();
 }
