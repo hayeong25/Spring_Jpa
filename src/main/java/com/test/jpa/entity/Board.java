f6dbf571dac8af47378b04e24992f58cef4186c1
hayeong25 <hayeong0624@gmail.com>
2024-05-30 14:07:02 +0900

    feat: Board 생성

---
+++ src/main/java/com/test/jpa/entity/Board.java
@@ -0,0 +1,28 @@
+package com.test.jpa.entity;
+
+import com.test.jpa.utils.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Board extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    @Column(length = 20, nullable = false)
+    private String writer;
+}
