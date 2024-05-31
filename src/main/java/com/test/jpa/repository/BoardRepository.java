7b9ca8027694a22d109e1fa3b5d01b2f2ec7b2e0
hayeong25 <hayeong0624@gmail.com>
2024-05-31 15:47:52 +0900

    test: QuerydslPredicateExecutor

--- src/main/java/com/test/jpa/repository/BoardRepository.java
+++ src/main/java/com/test/jpa/repository/BoardRepository.java
@@ -2,13 +2,16 @@ package com.test.jpa.repository;
 
 import com.test.jpa.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 /*
  * QueryDSL : @Entity가 붙은 엔터티들의 메타 정보를 담은 Q클래스 생성 및 Q클래스를 이용한 쿼리문 작성
  * 
  * QueryDSL 설정 후 build 하면 /build/classes/java/main...에 Q로 시작하는 엔티티 클래스 생성 -> 개발자가 건드리는 클래스 X. 내부 컬럼들이 변수 처리된 클래스
+ *
+ * QuerydslPredicateExecutor : QueryDSL 이용 시 상속 받는 인터페이스. 실무에서는 잘 사용하지 X
  */
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long>, QuerydslPredicateExecutor<Board> {
 
 }
--- src/test/java/com/test/jpa/BoardRepositoryTest.java
+++ src/test/java/com/test/jpa/BoardRepositoryTest.java
@@ -1,11 +1,18 @@
 package com.test.jpa;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.test.jpa.entity.Board;
+import com.test.jpa.entity.QBoard;
 import com.test.jpa.repository.BoardRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -42,5 +49,66 @@ public class BoardRepositoryTest {
 
             System.out.println("After Update : " + boardRepository.findAll());
         }
+    }
+
+    @Test
+    @DisplayName("QueryDSL 단일 항목 조회")
+    public void queryDSL1() {
+        IntStream.rangeClosed(1, 50).forEach(i -> {
+            Board board = Board.builder()
+                               .title("Title..." + i)
+                               .content("Content..." + i)
+                               .writer("Writer..." + i)
+                               .build();
+
+            boardRepository.save(board);
+        });
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        QBoard qBoard = QBoard.board;
+
+        // BooleanBuilder : WHERE 조건 컨테이너 (Predicate)
+        // cf. Predicate : 인자를 받아 boolean 반환하는 함수형 인터페이스
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+        // title LIKE '%3%'
+        // BooleanExpression은 null 반환 시 WHERE 절에서 조건 완전 무시된다
+        BooleanExpression expression = qBoard.title.contains("3");
+
+        booleanBuilder.and(expression);
+
+        // BoardRepository의 QuerydslPredicateExecutor로 findAll(Predicate, Pageable) 사용 가능
+        Page<Board> result = boardRepository.findAll(booleanBuilder, pageable);
+
+        result.stream().forEach(System.out::println);
+    }
+    
+    @Test
+    @DisplayName("QueryDSL 다중 항목 조회")
+    public void queryDSL2() {
+        IntStream.rangeClosed(1, 50).forEach(i -> {
+            Board board = Board.builder()
+                               .title("Title..." + i)
+                               .content("Content..." + i)
+                               .writer("Writer..." + i)
+                               .build();
+
+            boardRepository.save(board);
+        });
+
+        Pageable pageable = PageRequest.of(0, 13, Sort.by("bno").descending());
+
+        QBoard qBoard = QBoard.board;
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+        BooleanExpression expression = qBoard.title.contains("2").or(qBoard.content.contains("7"));
+
+        booleanBuilder.and(expression);
+
+        Page<Board> result = boardRepository.findAll(booleanBuilder, pageable);
+
+        result.stream().forEach(System.out::println);
     }
 }
