6b1af4553bd0dcea9189366c723320e2c7e842aa
hayeong25 <hayeong0624@gmail.com>
2024-05-31 14:55:39 +0900

    fix: JpaApplication과 @EnableJpaAuditing 중복 제거

--- src/main/java/com/test/jpa/config/JpaConfig.java
+++ src/main/java/com/test/jpa/config/JpaConfig.java
@@ -5,7 +5,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /*
  * @Config : QueryDSL 관련 설정
@@ -13,7 +12,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 
 @Configuration
-@EnableJpaAuditing
 public class JpaConfig {
 
     /*
